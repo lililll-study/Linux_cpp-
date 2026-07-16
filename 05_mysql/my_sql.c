22b74d34a281ad48435312fbd03249a4a9777cf9
lililll-study <342032684@qq.com>
2026-07-17 00:17:01 +0800

    7-17-1

--- 05_mysql/my_sql.c
+++ 05_mysql/my_sql.c
@@ -1,7 +1,7 @@
 
 #include <mysql.h>
 #include <stdio.h>
-
+#include <string.h>
 
 
 #define LHY_DB_SERVER_IP        "192.168.137.128"
@@ -10,10 +10,32 @@
 #define LHY_DB_SERVER_PWD       "123321"
 
 #define LHY_DB_DEFAULTDB       "LHY_DB"
+
+#define SQL_INSERT_TBL_USER     "INSERT TBL_USER(U_NAME, U_GENGDER) VALUES('LHY', 'man')"
+#define SQL_SELECT_TBL_USER     "SELECT * FROM TBL_USER;"
+
+
 // C U R D -->
 //
 
+int lhy_mysql_select(MYSQL *mysql){
+    // mysql_real_query --> sql
+    if (mysql_real_query(&mysql, SQL_SELECT_TBL_USER, strlen(SQL_SELECT_TBL_USER))){
+        // =0，表示成功
+        printf("mysql_real_query : %s\n", mysql_error(&mysql));
+        return -1;
+    }
+    // store -->
+    MYSQL_RES *res = mysql_store_result(mysql);
+    if (res == NULL) {
+        printf("mysql_store_query : %s\n", mysql_error(&mysql));
+        return -2;
+    }
+    // rows / fields
+    int rows = mysql_num_rows(res);
 
+    // fetch
+}
 
 
 int main() {
@@ -24,12 +46,19 @@ int main() {
         return -1;
     }
 
-    if (!mysql_real_connect(&mysql, LHY_DB_SERVER_IP, LHY_DB_SERVER_USER, LHY_DB_SERVER_PWD, LHY_DB_DEFAULTDB, LHY_DB_SERVER_PORT, NULL, 0)) {
+    if (!mysql_real_connect(&mysql, LHY_DB_SERVER_IP, LHY_DB_SERVER_USER,
+        LHY_DB_SERVER_PWD, LHY_DB_DEFAULTDB, LHY_DB_SERVER_PORT, NULL, 0)) {
         // =0, 表示连接失败
         printf("mysql_real_connect : %s\n", mysql_error(&mysql));
-        return -1;
+        return -2;
+    }
+    if (mysql_real_query(&mysql, SQL_INSERT_TBL_USER, strlen(SQL_INSERT_TBL_USER))){
+        // =0，表示成功
+        printf("mysql_real_query : %s\n", mysql_error(&mysql));
     }
 
+    mysql_close(&mysql);
+    return 0;
 }
 
 
